651855be0335618d8243fbcc69a8726305241817
Brian Tai <briantai24@gmail.com>
2019-04-17 01:54:55 -0700

    change route53 record to be a CNAME

--- route53.tf
+++ route53.tf
@@ -1,11 +1,9 @@
 resource "aws_route53_record" "this" {
   zone_id = "Z1Y4FEML24L94W"
   name    = "${local.env}-${local.region}"
-  type    = "A"
+  type    = "CNAME"
+  ttl     = "5"
+  records = ["${aws_elb.this.dns_name}"]
 
-  alias {
-    name                   = "dualstack.${aws_elb.this.dns_name}"
-    zone_id                = "${aws_elb.this.zone_id}"
-    evaluate_target_health = false
-  }
+  depends_on = ["aws_autoscaling_group.this"]
 }
